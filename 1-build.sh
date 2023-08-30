873b3dcb11cfb495a7123b746833d3f5657e8e8b
amanre <amanre556@gmail.com>
2023-08-30 22:32:34 +0200

    update

---
+++ 1-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+destination="/home/amanre/custom_repo/x86_64/"
+
+			makepkg -f
+
+			mv pywal-conky*pkg.tar.zst $destination
+
+			rm -r pkg
+			rm -r src
+			rm -rf pywal-conky*
+			echo "#############################################################################################"
+			echo "################  "$(basename `pwd`)" done"
+			echo "#############################################################################################"
+			
+		
